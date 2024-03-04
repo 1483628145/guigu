dde7d3b427fbe88ddcc69753477fd23c229878ef
LXB <14836628145l@gamil.com>
2024-03-04 14:54:07 +0800

    first commit

---
+++ src/settings.js
@@ -0,0 +1,20 @@
+// 页面基础设置
+module.exports = {
+
+  // 网站标题
+  title: '人力资源管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 固定头部 nav
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 侧边栏 Logo
+  sidebarLogo: true
+}
---
+++ src/store/getters.js
@@ -0,0 +1,10 @@
+// 模块属性
+const getters = {
+  // 取模块属性 便捷访问
+  sidebar: state => state.app.sidebar,
+  device: state => state.app.device,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name
+}
+export default getters
