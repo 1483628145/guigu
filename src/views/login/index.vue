5c860b59d81e09bbb0159e41d848dd538db1368f
LXB <14836628145l@gamil.com>
2024-03-04 15:35:22 +0800

    登录表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,20 +5,20 @@
       <h1>登录</h1>
       <el-card shadow="never" class="login-card">
         <!--登录表单-->
-        <el-form>
-          <el-form-item>
-            <el-input placeholder="请输入手机号" />
+        <el-form ref="form" :rules="LoginRules" :model="LoginForm">
+          <el-form-item prop="mobile">
+            <el-input v-model="LoginForm.mobile" placeholder="请输入手机号" />
           </el-form-item>
-          <el-form-item>
-            <el-input placeholder="请输入密码" />
+          <el-form-item prop="password">
+            <el-input v-model="LoginForm.password" placeholder="请输入密码" show-password />
           </el-form-item>
-          <el-form-item>
-            <el-checkbox>
+          <el-form-item prop="isOk">
+            <el-checkbox v-model="LoginForm.isOk">
               用户平台使用协议
             </el-checkbox>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" style="width: 350px;">
+            <el-button type="primary" style="width: 350px;" @click="Login">
               登录
             </el-button>
           </el-form-item>
@@ -28,8 +28,74 @@
   </div>
 </template>
 <script>
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  data() {
+    return {
+      // 表单数据
+      LoginForm: {
+        mobile: '',
+        password: '',
+        isOk: false
+      },
+      /* 表单校验规则 */
+      LoginRules: {
+        mobile: [
+          // 离开焦点触发 必须输入内容
+          {
+            required: true,
+            message: '请输入手机号',
+            trigger: 'blur'
+          },
+          // 格式校验
+          {
+            pattern: /^1[3456789]\d{9}$/,
+            message: '手机号格式不正确',
+            trigger: 'blur'
+
+          }
+        ],
+        password: [
+          // 离开焦点触发 必须输入内容
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          },
+          // 格式校验
+          {
+            pattern: /^.{6,16}$/,
+            message: '密码长度在6-16之间',
+            trigger: 'blur'
+          }
+        ],
+        // 用户使用协议
+        isOk: [
+          // 自定义校验规则
+          {
+            validator: (rule, value, callback) => {
+              // rule: 规则
+              // value 校验的值
+              // callback 成功的回调
+              value ? callback() : callback(new Error('请勾选用户使用协议'))
+            }
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 登录按钮
+    Login() {
+      // 登录之前做表单校验
+      this.$refs.form.validate((isOk) => {
+        if (isOk) {
+          alert('pass')
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="scss">
